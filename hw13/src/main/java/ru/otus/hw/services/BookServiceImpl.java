64beb8ce042e77c6d571628103b6a99f2511d9a7
ily.galochkin@x5.ru <ily.galochkin@x5.ru>
2025-05-22 12:46:43 +0300

    hw13

---
+++ hw13/src/main/java/ru/otus/hw/services/BookServiceImpl.java
@@ -0,0 +1,111 @@
+package ru.otus.hw.services;
+
+import java.util.List;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.dto.BookShortDTO;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.models.Author;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Genre;
+import ru.otus.hw.repositories.AuthorRepository;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.GenreRepository;
+
+import static org.springframework.util.CollectionUtils.isEmpty;
+
+@RequiredArgsConstructor
+@Service
+public class BookServiceImpl implements BookService {
+    private final AuthorRepository authorRepository;
+
+    private final GenreRepository genreRepository;
+
+    private final BookRepository bookRepository;
+
+    private final AclServiceWrapperService aclServiceWrapperService;
+
+    @Override
+    @Transactional(readOnly = true)
+    @PostAuthorize("hasPermission(returnObject, 'READ')")
+    public Book getById(long id) {
+        return bookRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Book not found"));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    @PostFilter("hasPermission(filterObject, 'READ')")
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    @PreAuthorize("hasRole('ADMIN')")
+    public Book insert(BookShortDTO bookShortDTO) {
+        Book newBook = save(0, bookShortDTO.getTitle(), bookShortDTO.getAuthorId(), bookShortDTO.getGenresIds());
+        aclServiceWrapperService.createPermission(newBook);
+        return newBook;
+    }
+
+    @Transactional
+    @Override
+    @PreAuthorize("hasPermission(#bookDTO.id, 'ru.otus.hw.models.Book', 'WRITE')")
+    public Book update(@Param("bookDTO") BookShortDTO bookDTO) {
+        var book = getBookById(bookDTO.getId());
+        var author = getAuthorById(bookDTO.getAuthorId());
+        var genres = getGenresByIds(bookDTO.getGenresIds());
+        book.setTitle(bookDTO.getTitle());
+        book.setAuthor(author);
+        book.setGenres(genres);
+        return bookRepository.save(book);
+    }
+
+    @Override
+    @Transactional
+    @PreAuthorize("hasRole('ADMIN')")
+    public void deleteById(long id) {
+        bookRepository.deleteById(id);
+    }
+
+    private Book save(long id, String title, long authorId, Set<Long> genresIds) {
+        if (CollectionUtils.isEmpty(genresIds)) {
+            throw new IllegalArgumentException("Genres ids must not be null");
+        }
+
+        var author = authorRepository.findById(authorId)
+                .orElseThrow(() -> new EntityNotFoundException("Author with id %d not found".formatted(authorId)));
+        var genres = genreRepository.findAllByIdIn(genresIds);
+        if (CollectionUtils.isEmpty(genres) || genresIds.size() != genres.size()) {
+            throw new EntityNotFoundException("One or all genres with ids %s not found".formatted(genresIds));
+        }
+
+        var book = new Book(id, title, author, genres);
+        return bookRepository.save(book);
+    }
+
+    private Book getBookById(long bookId) {
+        return bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("Book with id %d not found".formatted(bookId)));
+    }
+
+    private Author getAuthorById(long authorId) {
+        return authorRepository.findById(authorId)
+                .orElseThrow(() -> new EntityNotFoundException("Author with id %d not found".formatted(authorId)));
+    }
+
+    private List<Genre> getGenresByIds(Set<Long> genresIds) {
+        var genres = genreRepository.findAllByIdIn(genresIds);
+        if (isEmpty(genres) || genresIds.size() != genres.size()) {
+            throw new EntityNotFoundException("One or all genres with ids %s not found".formatted(genresIds));
+        }
+        return genres;
+    }
+}
